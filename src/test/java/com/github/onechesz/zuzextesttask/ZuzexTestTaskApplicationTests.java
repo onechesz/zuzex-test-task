b70ab3609a0f11a8afb588bf77fcef7598ca3777
Ivan Minyaev <i.onechesz@gmail.com>
2023-08-25 19:09:13 +0300

    Initial commit

---
+++ src/test/java/com/github/onechesz/zuzextesttask/ZuzexTestTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.onechesz.zuzextesttask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZuzexTestTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
